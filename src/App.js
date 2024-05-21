84541de70be51d0051867c87b7e11dd8430bc054
Elena <emkhuzhina@gmail.com>
2024-05-22 00:05:58 +0530

    update init files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
       </header>
     </div>
   );
